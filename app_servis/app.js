9c7a7f25abf93642bd698701705790bf3b1e83db
Vasilije <vraicevic9022rn@raf.rs>
2023-10-26 09:54:55 +0200

    rm node_modeules

--- app_servis/app.js
+++ app_servis/app.js
@@ -3,18 +3,20 @@ const path = require('path');
 
 const app = express();
 
+const BP = require('body-parser');
+
+
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
 })
 
-app.post("/novo-jelo", (req, res) => {
+app.post("/novo_vozilo_ponuda", (req, res) => {
     res.send(req.body);
 })
 
-const BP = require('body-parser');
 
-app.use('/novo-jelo', BP.urlencoded({ extended: false }));
+app.use('/novo_vozilo_ponuda', BP.urlencoded({ extended: false }));
 
 app.listen(8000);
